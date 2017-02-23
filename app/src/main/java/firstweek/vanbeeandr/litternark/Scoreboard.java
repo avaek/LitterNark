484dbadb614786139ab5dff40b290f1953b66646
Jacob Hoffland <hofflajaco@myvuw.ac.nz>
2017-02-23 14:27:02 +1300

    added matchTables method to Scoreboard class

--- app/src/main/java/firstweek/vanbeeandr/litternark/Scoreboard.java
+++ app/src/main/java/firstweek/vanbeeandr/litternark/Scoreboard.java
@@ -1,5 +1,8 @@
 package firstweek.vanbeeandr.litternark;
 
+import android.app.Activity;
+import android.widget.TextView;
+
 /**
  * Created by hofflajaco on 23/02/2017.
  */
@@ -82,5 +85,51 @@ public class Scoreboard {
         }
 
         return newScores;
+    }
+
+    public static void matchTable(String[][] scoreArray, Activity act){
+
+        TextView tv11 = (TextView) act.findViewById(R.id.comp_1);
+        TextView tv21 = (TextView) act.findViewById(R.id.comp_2);
+        TextView tv31 = (TextView) act.findViewById(R.id.comp_3);
+        TextView tv41 = (TextView) act.findViewById(R.id.comp_4);
+        TextView tv51 = (TextView) act.findViewById(R.id.comp_5);
+        TextView tv61 = (TextView) act.findViewById(R.id.comp_6);
+        TextView tv71 = (TextView) act.findViewById(R.id.comp_7);
+        TextView tv81 = (TextView) act.findViewById(R.id.comp_8);
+        TextView tv91 = (TextView) act.findViewById(R.id.comp_9);
+        TextView tv101 = (TextView) act.findViewById(R.id.comp_10);
+        TextView tv12 = (TextView) act.findViewById(R.id.narks_1);
+        TextView tv22 = (TextView) act.findViewById(R.id.narks_2);
+        TextView tv32 = (TextView) act.findViewById(R.id.narks_3);
+        TextView tv42 = (TextView) act.findViewById(R.id.narks_4);
+        TextView tv52 = (TextView) act.findViewById(R.id.narks_5);
+        TextView tv62 = (TextView) act.findViewById(R.id.narks_6);
+        TextView tv72 = (TextView) act.findViewById(R.id.narks_7);
+        TextView tv82 = (TextView) act.findViewById(R.id.narks_8);
+        TextView tv92 = (TextView) act.findViewById(R.id.narks_9);
+        TextView tv102 = (TextView) act.findViewById(R.id.narks_10);
+
+        tv11.setText(scoreArray[0][0]);
+        tv21.setText(scoreArray[1][0]);
+        tv31.setText(scoreArray[2][0]);
+        tv41.setText(scoreArray[3][0]);
+        tv51.setText(scoreArray[4][0]);
+        tv61.setText(scoreArray[5][0]);
+        tv71.setText(scoreArray[6][0]);
+        tv81.setText(scoreArray[7][0]);
+        tv91.setText(scoreArray[8][0]);
+        tv101.setText(scoreArray[9][0]);
+        tv12.setText(scoreArray[0][1]);
+        tv22.setText(scoreArray[1][1]);
+        tv32.setText(scoreArray[2][1]);
+        tv42.setText(scoreArray[3][1]);
+        tv52.setText(scoreArray[4][1]);
+        tv62.setText(scoreArray[5][1]);
+        tv72.setText(scoreArray[6][1]);
+        tv82.setText(scoreArray[7][1]);
+        tv92.setText(scoreArray[8][1]);
+        tv102.setText(scoreArray[9][1]);
+
     }
 }
