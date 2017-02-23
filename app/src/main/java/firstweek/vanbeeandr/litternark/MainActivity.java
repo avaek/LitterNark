6a2e8f38543ff893a7c2dfedd5982b6a5a82b7ee
James Voong <voongjame@AD.ECS.VUW.AC.NZ>
2017-02-23 13:59:04 +1300

    added mcdsBtn and made it work partially

--- app/src/main/java/firstweek/vanbeeandr/litternark/MainActivity.java
+++ app/src/main/java/firstweek/vanbeeandr/litternark/MainActivity.java
@@ -9,11 +9,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static firstweek.vanbeeandr.litternark.R.id.mcdBtn;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -63,9 +66,21 @@ public class MainActivity extends AppCompatActivity {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
+            companySelectActivity();
         }
 //        dispatchSavePictureIntent();
+    }
+
+    private void companySelectActivity() {
+        setContentView(R.layout.company_select);
+        ImageButton mcdsBtn = (ImageButton) findViewById(mcdBtn);
+        mcdsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.gallery);
+            }
+        });
+
     }
 
 
